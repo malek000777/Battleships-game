ec6494e5fc31fd393ca01e9e32d09a74dd120f04
malek 000777 <malek.h000777@gmail.com>
2024-06-09 09:31:44 +0000

    Add create_grid function to generate the grid

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+
+# Function to create a grid
+def create_grid(size):
+    return [['O' for _ in range(size)] for _ in range(size)]
